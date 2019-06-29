7e5061dae8c6bb1f8e4c50a583de44b4595c0d85
borisff <b.eduardoff@gmail.com>
2019-06-29 16:51:25 -0400

    Avance para el trabajo final bajamos las fechas y la temperatura.

---
+++ Weather.R
@@ -0,0 +1,28 @@
+#install.packages("rvest")
+library(rvest)
+
+htmlWeather <- "https://weather.com/es-CL/tiempo/mensual/l/cb0b09b804fbcfb93b3485607c771c23607ce00484606431defb3c0cd10efb79"
+paginaWeather <- read_html(htmlWeather)
+
+#teniendo la informacion realizamos la extraccion 
+nodesDelHTMLWeatherDia <- html_nodes(paginaWeather,".date")
+
+#solo trabajamos con el texto
+dia <- html_text(nodesDelHTMLWeatherDia)
+#--------------------------------------------------------------------
+
+#teniendo la informacion realizamos la extraccion 
+nodesDelHTMLWeatherTemperaturaAlta <- html_nodes(paginaWeather,".temps")
+
+#trabajamos con la temperatura
+temperatura <- html_text(nodesDelHTMLWeatherTemperaturaAlta)
+
+#eliminamos los "°" de lo numeros 
+temperaturaLimpia <- gsub("°",";",temperatura)
+
+#creamos la tabla 
+tablaDatos <- data.frame(dias = dia, temperaturas = temperaturaLimpia)
+
+#intentamos separar la temperatura en dos columnas una mayor temp y una menor temp.
+library(stringr)
+str_split_fixed(tablaDatos$temperaturas,";")
