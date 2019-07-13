7efc5200eb9395424108e3b1e882bea34bd7404d
borisff <b.eduardoff@gmail.com>
2019-07-13 11:02:01 -0400

    realizamos la extraccion de una base

--- Weather.R
+++ Weather.R
@@ -33,7 +33,7 @@ tablaDatos <- data.frame(dias = dia, temperaturas = temperaturaLimpia)
 ###############################################################################
 
 #guardo los datos
-write.table(tablaDatos, file="tablajulio12.csv", sep = ";")
+write.table(tablaDatos, file="tablajulio13.csv", sep = ";")
 
 #################################################################################
 
