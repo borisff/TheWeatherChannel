0beb3c9a275de95961ef6b9291b9d5cc32e475c0
borisff <b.eduardoff@gmail.com>
2019-07-31 13:40:45 -0400

    datos descargados con api

--- Api.R
+++ Api.R
@@ -17,8 +17,28 @@ contentNuevo <- content(consultaGetNuevas,"text")
 
 jsonNuevo <- jsonlite::fromJSON(contentNuevo)
 
+write_json(jsonNuevo,"todosLosDatos31deJunio")
+
+#################################################################################
+############################# Del día ###########################################
+################################################################################
+
+token = 'd522aa97197fd864d36b418f39ebb323'
+forma = 'json'
+geoco = "-33.45%2C-70.67"
+lengu = 'es-CL'
+units = 'm'
+
+urlClima <- paste('https://api.weather.com/v2/turbo/vt1observation',token,'&format=',forma,'&geocode=',geoco,'&language=',lengu,'&units=', units,sep = "")
+print(urlClimaDia)
+
+consultaGetNuevasDia <- GET(url = urlClima,format = 'json')#, query = list(apiKey = token, format = forma, geocode = geoco, language = lengu, units = units))  
+contentNuevo <- content(consultaGetNuevas,"text")
 
 
+jsonNuevo <- jsonlite::fromJSON(contentNuevo)
+
+write_json(jsonNuevo,"datoDelDia31DeJulio")
 
 
 
