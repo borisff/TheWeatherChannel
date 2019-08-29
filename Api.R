968de3e204377d9ad84487519d21cd20c389750b
borisff <b.eduardoff@gmail.com>
2019-08-28 22:42:08 -0400

    mas datos

--- Api.R
+++ Api.R
@@ -17,5 +17,5 @@ contentNuevo <- content(consultaGetNuevas,"text")
 
 jsonNuevo <- jsonlite::fromJSON(contentNuevo)
 
-write_json(jsonNuevo,"todosLosDatos26DeAgosto")
+write_json(jsonNuevo,"todosLosDatos27DeAgosto")
 
